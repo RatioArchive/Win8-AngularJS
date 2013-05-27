c1bc06e91cee5eadc11542d083dde70e769188f9
Ben McEldowney <ben@ratiointeractive.com>
2013-05-27 09:05:25 -0700

    Creating todo module

--- Win8-AngularJS/js/todo.js
+++ Win8-AngularJS/js/todo.js
@@ -1,29 +1,31 @@
-﻿function TodoCtrl($scope) {
-    /// <param name="$scope" type="angular.Scope">The injected scope.</param>
-    
-    $scope.todos = [
-        { text: 'learn angular', done: true },
-        { text: 'integrate angular into metro app', done: true },
-        { text: 'build an angular app', done: false }];
-    
-    $scope.addTodo = function () {
-        $scope.todos.push({ text: $scope.todoText, done: false });
-        $scope.todoText = '';
-    };
+﻿angular.module('todo', [])
+    .controller('TodoCtrl', function ($scope) {
+        /// <param name="$scope" type="angular.Scope">The injected scope.</param>
 
-    $scope.remaining = function () {
-        var count = 0;
-        angular.forEach($scope.todos, function (todo) {
-            count += todo.done ? 0 : 1;
-        });
-        return count;
-    };
+        $scope.todos = [
+            { text: 'learn angular', done: true },
+            { text: 'integrate angular into metro app', done: true },
+            { text: 'build an angular app', done: false }];
 
-    $scope.archive = function () {
-        var oldTodos = $scope.todos;
-        $scope.todos = [];
-        angular.forEach(oldTodos, function (todo) {
-            if (!todo.done) $scope.todos.push(todo);
-        });
-    };
-}
+        $scope.addTodo = function () {
+            $scope.todos.push({ text: $scope.todoText, done: false });
+            $scope.todoText = '';
+        };
+
+        $scope.remaining = function () {
+            var count = 0;
+            angular.forEach($scope.todos, function (todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        $scope.archive = function () {
+            var oldTodos = $scope.todos;
+            $scope.todos = [];
+            angular.forEach(oldTodos, function (todo) {
+                if (!todo.done) $scope.todos.push(todo);
+            });
+        };
+
+    })
